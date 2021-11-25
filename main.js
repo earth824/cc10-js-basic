ebeecc3b52cea34dd6092ec5bff180222ccacaa2
pisutthee <pisut.flyinggiraffe@gmail.com>
2021-11-25 16:40:42 +0700

    complete lesson

---
+++ main.js
@@ -0,0 +1,410 @@
+// เสมือนว่า ประกาศตั้งแต่แรก
+// var d, h;
+// var a = 20;
+
+// คำสั่ง alert จะแสดงกล่องข้อความใน browser
+// alert('My name is John');
+// console.log(a);
+// console.log(d);
+// let a = 20;
+// let b = 30;
+// let c = a + b;
+// a = 100;
+// var d = 'Hello';
+// const e = 'World';
+// // e = 'abcd';
+// f = 'abcd';
+// console.log('Thailand');
+// console.log(c);
+// console.log(f);
+
+// let g;
+// var h;
+// const i = 0;
+
+// {
+// }
+
+// function sum() {
+//   var z = 20;
+// }
+
+// // let a = 500;
+// var d = 'Good bye';
+
+// break เป็น keyword
+// const break = '';
+
+// ห้ามขึ้นต้นด้วยตัวเลข
+// const 9abc = '';
+
+// const _ = '_';
+// const $ = '$';
+// const acv& = '%';
+// const title1 = ''
+
+// const user
+// camel case
+// const myIncomeTransaction = 0;
+// // snake case
+// const my_income_transaction = 0;
+// // pascal case
+// const MyIncomeTransaction = 0;
+
+// Lab 3.1
+// const name = 'John';
+// let person;
+// person = name;
+// alert(person);
+
+// Lab 3.2
+// let money, name, address, age;
+
+// Lab 3.3
+// const user = 'iamhero';
+// const role = 'customer';
+// alert(username);
+
+// Lab 3.4
+// const firstName = 'John';
+// let lastName = 'Doe';
+// var nickname = 'J';
+
+// const firstName = 'Joe';
+// let lastName = 'Biden';
+// var nickname = 'JB';
+
+// birthDate = '1 Jan 1990';
+
+// Lab 3.5
+// const BRAND_NAME = 'I am a hero';
+
+// let num = 20;
+// let result = num * 5;
+// let a = NaN;
+// console.log(a + 1);
+// let inf = Infinity;
+// let b = 1 / 0;
+// console.log(b);
+
+// let firstName = 'Jonh';
+// let lastName = 'Doe';
+// let nickname = `JD`;
+
+// let fullname = 'My name is ' + firstName + ' ' + lastName;
+// console.log(fullname);
+// let fullname1 = `My name is ${firstName} ${lastName} ${100 * 20 - 35}`;
+// console.log(fullname1);
+
+// const fullname = "I'm a hero";
+// const fullname = 'I\'m a hero';
+
+// const text = 'He say "Hello"';
+// let message = 'My name is ${firstName} ${lastName}';
+
+// console.log(typeof text);
+// console.log(typeof(text));
+// console.log(typeof 10);
+// console.log(typeof true);
+
+// Lab 4.1
+// number is 2
+// result is 4
+// I live in Thailand
+// I live in Thailand, Asia
+// I live in Thailand, Asia
+// I live in country, continent
+
+// Lab 4.2
+// const firstName = 'John';
+// const lastName = 'Doe';
+// const age = 20;
+// const address = '502 Mint Tower Banthadthong';
+// console.log(`Full Name:${firstName} ${lastName}, Age: ${age}, Address: ${address}`);
+
+// Lab 4.3
+// string
+// undefined
+// string
+// number
+// number
+// string
+// number
+// object
+
+// alert('Hello');
+// console.log(typeof null); // object
+// console.log(typeof alert); // function
+// console.log(typeof (1 + '3'));
+
+// let num = 100;
+// let boo = false;
+
+// const strNum = String(num);
+// const strNum2 = '' + num;
+// const strNum3 = `${num}`;
+
+// const strBoo = '' + boo;
+// console.log(typeof strNum);
+// console.log(typeof strNum2);
+// console.log(typeof strNum3);
+// console.log(typeof strBoo);
+// console.log(strBoo);
+// console.log(boo);
+// console.log(num + 200); // 300
+// console.log(strNum + 200); // 100200
+
+// let str = '   400    '; // => '400'
+// console.log(Number(str));
+// let str1 = '  4  0  0    '; // => '4  0  0'
+// console.log(Number(str1));
+// let str2 = '                                '; // ''
+// console.log(Number(str2));
+
+// let a;
+// console.log(Number(a));
+// let b = null;
+// console.log(+b);
+// let c = true;
+// console.log(c * 1);
+
+// let input = prompt('Enter something', 0);
+// console.log(input);
+
+// let a = 'Hello';
+// let b = 200;
+// console.log(a * b); // => NaN * 200
+// console.log(Math.max(100, '', null, '70' + '50')); // convert to 100, 0, 0, 7050
+
+// let a = 50;
+// let b;
+// let c = null;
+// let d = 3 * '  3 0  ';
+// console.log(Boolean(a));
+// console.log(!!b);
+// console.log(!!c);
+// console.log(Boolean(d));
+// console.log(Boolean('0'));
+
+// let a = false;
+// let b = false;
+
+// // AND (&&)
+// let c = a && b; // เป็น true เมื่อ A และ B เป็น true ทั้งคู่ ที่เหลือเป็น false
+// // OR (||)
+// let d = a || b; // เป็น false เมื่อ A และ B เป็น false ทั้งคู่ ที่เหลือเป็น true
+// // NOT (!)
+// let e = !a // เป็นค่าตรงกันข้ามกับค่าเดิม
+
+// let a = 20 - 5;
+// let c = null;
+// let d = +c;
+
+// console.log(0.2 ** 3);
+
+// let a = 3;
+// // a = a + 1;
+// // a++;
+// // a--;
+// console.log(a++);
+// // ได้ผลลัพธ์ตามด้านล่าง
+// console.log(a);
+// a = a + 1;
+
+// console.log(++a);
+// // ได้ผลลัพธ์ตามด้านล่าง
+// a = a + 1;
+// console.log(a);
+
+// let num = 3;
+// num += 7;
+// num -= 4; // num = num - 4
+// num *= 3; // num = num * 3
+// num /= 2; // num = num / 2
+
+// Lab 6.1
+// let a = undefined;
+// let d = String(a);
+
+// let b = 2;
+// let e = '' + b;
+
+// let c = a++;
+// let f = `${c}`;
+
+// console.log(d, e, f);
+
+// Lab 6.2
+// let a = undefined;
+// let b = null;
+// let c = b + '4 2';
+
+// let d = Number(a);
+// let e = +b;
+// let f = c * 1;
+
+// console.log(d, e, f);
+
+// Lab 6.3
+// let a = undefined;
+// let b = ' ';
+// let c = !b;
+
+// let d = Boolean(a);
+// let e = !!b;
+// let f = !!c;
+// console.log(d, e, f);
+
+// Lab 6.5
+// let a = 0;
+// a++;
+// a += 3;
+// a *= 17;
+// a %= 7;
+// console.log(a);
+
+// Lab 6.6
+// Line1: a = 1
+// Line2: b = 2
+// Line3: a = 2, b = 2, c = 1
+// Line4: a = 2, b = 2, c = 2, d = 2;
+// Line5: a = 2, b = 2, c = 2, d = 4, e = 10;
+
+// Lab 6.7
+// const num1 = +prompt('Enter first number');
+// const num2 = +prompt('Enter second number');
+
+// alert(num1 + num2);
+
+// Lab 6.8
+// const a = 0.1;
+// const b = 0.2;
+// const c = a + b;
+
+// console.log(c.toFixed(1));
+// console.log(c.toPrecision(1));
+
+// console.log((5.8944).toFixed(2));
+// console.log((5.8944).toPrecision(2));
+
+// Animal กับ Plant เปรียบเทียบ A กับ P => Animal < Plant
+// Animal กับ Any เปรียบเทียบ A กับ A => เท่ากัน, เปรียบเทียบ n กับ n => เท่ากัน, เปรียบเทียบ i กับ y => Animal < Any
+// Man กับ Mansome => Man < Mansome
+
+// 1 == '1' => true
+// 1 === '1' => false
+
+// console.log(NaN === NaN);
+// console.log(Object.is(NaN, NaN));
+
+const gender = 'F';
+
+// if (gender === 'M') {
+//   alert('Gentleman')
+//   console.log('Gentleman')
+// }
+
+// if (gender === 'M') alert('Gentleman');console.log('Gentleman');
+
+// if (gender === 'M') {
+//   alert('gentleman');
+// } else {
+//   alert('Lady');
+// }
+
+// if (gender === 'M') {
+//   alert('Gentleman')
+// } else if (gender === 'F') {
+//   alert('Lady')
+// } else {
+//   alert('OMG')
+// }
+
+const num = 20;
+// let text;
+
+// if (num % 2 === 0) {
+//   text = 'Even';
+// } else {
+//   text = 'Odd';
+// }
+
+// text = num % 2 === 0 ? 'Even' : 'Odd';
+// text = num % 2 === 0 ? (num % 3 === 0 ? 'หาร 3 ลงตัว' : 'หาร 3 ไม่ลงตัว') : 'Odd';
+
+// let username = promp('Enter username');
+
+// if (username === null || username === undefined) {
+//   username = 'Guest';
+// }
+
+// username = username ?? 'Guest';
+
+// const age = 35;
+// const guessAge = +prompt('Please guess my age');
+
+// let showMessage = '';
+
+// if (guessAge === 35) {
+//   showMessage = 'ถูกต้อง';
+// } else if (guessAge === 34) {
+//   showMessage = 'น้อยไป';
+// } else if (guessAge === 36) {
+//   showMessage = 'มากไป';
+// } else {
+//   showMessage = 'ไม่ใกล้เลย';
+// }
+
+// switch (guessAge) {
+//   case 35: {
+//     showMessage = 'ถูกต้อง';
+//     alert('35');
+//     break;
+//   }
+//   case 34: {
+//     showMessage = 'น้อยไป';
+//     alert(34);
+//     break;
+//   }
+//   case 36: {
+//     showMessage = 'มากไป';
+//     alert(36);
+//     break;
+//   }
+//   default:
+//     showMessage = 'ไม่ใกล้เลย';
+//     alert('default');
+// }
+
+// let i = 0;
+// while (i < 10) {
+//   console.log(i);
+//   i++;
+// }
+
+// i = 0;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i < 10);
+
+// for (let i = 0; i < 10; i++) {
+//   if (i % 2 === 1) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// console.log(i);
+
+// let i = 10;
+// for (; i < 15; ) {
+//   console.log(i);
+//   i = i +5
+// }
+
+const boolean = 'a' || 9 || 'bngh' || null;
+const boolean1 = null || undefined || 0 || '' || NaN;
+console.log(boolean);
+const an = 20 && 'undefined' && 'Hello' && 'null';
+console.log(an);
